438588ef1e6b49e9dfde2cf38098f599c2927f5e
Zhong Tan <zhtgeneral@gmail.com>
2024-05-03 15:45:11 -0700

    added tests for front end login page

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,24 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('login page', () => {
+  it.only('loads brand', () => {
+    cy.visit('/')
+    cy.get('h2').should('have.text', 'Messenger clone')
+  })
+
+  it('loads sign in page', () => {
+    cy.visit('/')
+    cy.get('button[type="submit"]').should('have.text', 'Sign in')
+    cy.get('input').then($inputs => {
+      expect($inputs).to.have.length(2);
+    });
+  })
+
+  it('loads register page', () => {
+    cy.visit('/')
+    cy.contains('Create an account').click().then(() => {
+      cy.get('button[type="submit"]').should('have.text', 'Register')
+      cy.get('input').then($inputs => {
+        expect($inputs).to.have.length(3);
+      });
+    })
   })
 })
