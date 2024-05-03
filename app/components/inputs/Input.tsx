743ddad64a0b21d0acaae8fd28a15efa6c0f0db7
Zhong Tan <zhtgeneral@gmail.com>
2024-05-03 15:15:12 -0700

    login form without backend

---
+++ app/components/inputs/Input.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import clsx from "clsx";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  label    : string;
+  id       : string;
+  type?    : string;
+  required?: boolean;
+  disabled?: boolean;
+  register : UseFormRegister<FieldValues>;
+  errors   : FieldErrors
+}
+
+const Input: React.FC<InputProps> = ({label, id, type, disabled, required, register, errors}) => {
+  return (
+    <div>
+      <label className='block text-sm font-medium leading-6 text-gray-900' htmlFor={id}>
+        {label}
+      </label>
+      <div className="mt-2">
+        <input id={id} type={type} autoComplete={id} disabled={disabled} {...register(id, {required})} 
+        className={clsx('form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6',
+          errors[id] && 'focus:ring-rose-500',
+          disabled   && 'cursor-default opacity-50'
+        )}/>
+
+      </div>
+    </div>
+  )
+}
+
+export default Input
