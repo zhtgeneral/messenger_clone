b8a3974699d8c0c51e944a10c17170c0598e9a6d
Zhong Tan <zhtgeneral@gmail.com>
2024-06-04 12:42:58 -0700

    fixed logging out from different pages

--- app/components/DesktopItem.tsx
+++ app/components/DesktopItem.tsx
@@ -22,8 +22,8 @@ const DesktopItem: React.FC<DesktopItemProps> = ({
   return (
     <li onClick={handleClick}>
       <Link 
-        id="desktopItem"
-        href={href} 
+        id       ="desktopItem"
+        href     ={href} 
         className={
           clsx('group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100',
           active && 'bg-gray-100 text-black')
