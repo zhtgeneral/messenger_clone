74fcf91573bf09bf2ace585a909ecb9ade661939
Zhong Tan <zhtgeneral@gmail.com>
2024-06-12 17:01:38 -0700

    tested home page

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,24 +1,28 @@
-describe('loads login page', () => {
-  it('loads brand', () => {
+describe('page functions', () => {
+  it('displays brand', () => {
     cy.visit('/')
     cy.get('h2').should('have.text', 'Messenger clone')
   })
 
-  it('loads sign in page', () => {
+  it('renders sign in page', () => {
     cy.visit('/')
     cy.get('button[type="submit"]').should('have.text', 'Sign in')
     cy.get('input').then($inputs => {
       expect($inputs).to.have.length(2);
+      cy.get('body').should('contain.text', 'Email')
+      cy.get('body').should('contain.text', 'Password')
     });
   })
 
-  it('loads register page', () => {
+  it('renders register page', () => {
     cy.visit('/')
-    cy.contains('Create an account').click().then(() => {
-      cy.get('button[type="submit"]').should('have.text', 'Register')
-      cy.get('input').then($inputs => {
-        expect($inputs).to.have.length(3);
-      });
-    })
+    cy.contains('Create an account').click()
+    cy.get('button[type="submit"]').should('have.text', 'Register')
+    cy.get('input').then($inputs => {
+      expect($inputs).to.have.length(3);
+      cy.get('body').should('contain.text', 'Name')
+      cy.get('body').should('contain.text', 'Email')
+      cy.get('body').should('contain.text', 'Password')
+    });
   })
 })
