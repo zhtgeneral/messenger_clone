a1199d45dddbe6c33f2ee01e94fac1b4f92eea70
Zhong Tan <zhtgeneral@gmail.com>
2024-06-12 17:06:41 -0700

    tested login functions

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,39 +1,26 @@
-const         email = Cypress.env('EMAIL');
-const      password = Cypress.env('PASSWORD');
-const wrongPassword = password?.toUpperCase();
-const loginUrl = 'api/auth/callback/credentials';
+describe('login functions', () => {
+  const testEmail     = Cypress.env('TEST_EMAIL')
+  const testPassword  = Cypress.env('TEST_PASSWORD')
+  const wrongPassword = testPassword?.toUpperCase();
+  const loginAPI = 'api/auth/callback/credentials';
 
-describe('handles login', () => {
   it('fails with wrong password', () => {
-    cy.intercept('POST', loginUrl).as('login');
-
-    cy.login(email, wrongPassword);
-
+    cy.intercept('POST', loginAPI).as('login');
+    cy.login(testEmail, wrongPassword);
     cy.wait('@login').then((intercept) => {
       expect(intercept.response?.statusCode).to.equal(401);
     })
-
     cy.get('div[role="status"]').should('contain.text', 'Invalid Credentials');
   })
-  it('logins with correct credentials', () => {
-    cy.intercept('POST', loginUrl).as('login');
 
-    cy.login(email, password);
-    
+  it('login UX', () => {
+    cy.intercept('POST', loginAPI).as('login');
+    cy.login(testEmail, testPassword);
     cy.wait('@login').then((intercept) => {
       expect(intercept.response?.statusCode).to.equal(200);
     })
+    cy.get('div[role="status"]').should('contain.text', 'Logged in');
+    cy.location('pathname').should('eq', '/users');
   })
-  it('redirects logged in user to users page', () => {
-    cy.intercept('POST', loginUrl).as('login');
-
-    cy.login(email, password);
-    
-    cy.wait('@login').then((intercept) => {
-      expect(intercept.response?.statusCode).to.equal(200);
-    })
-    cy.url().should('eq', `${Cypress.env('NEXT_PUBLIC_DOMAIN')}/users`)
-  })
-
   // skip mocks for third party logins cuz its a pain
 })
