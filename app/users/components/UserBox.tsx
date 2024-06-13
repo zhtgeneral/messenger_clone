825a656c0349101729359bcf985586de9830ea30
Zhong Tan <zhtgeneral@gmail.com>
2024-06-12 17:21:24 -0700

    tested users page

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -26,7 +26,7 @@ const Userbox: React.FC<UserboxProps> = ({
     .finally(() => setIsLoading(false))
   }, [data, router])
   return (
-    <div onClick={handleClick} className='w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'>
+    <div id="userBox" onClick={handleClick} className='w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer'>
       <Avatar user={data}/>
       <div className='min-w-0 flex-1'>
         <div className='focus:outline-none'>
--- cypress/e2e/users.cy.ts
+++ cypress/e2e/users.cy.ts
@@ -1,37 +1,34 @@
-describe('users route security', () => {
-  it('is protected for unauthenticated users', () => {
-    cy.visit('/users').then(() => {
-      cy.url().should('eq', `${Cypress.env('NEXT_PUBLIC_DOMAIN')}/?callbackUrl=%2Fusers`)
-    })
-  })
-  it('allows authenticated users', () => {
-    cy.login(Cypress.env('EMAIL'), Cypress.env('PASSWORD'))
-    cy.url().should('eq', `${Cypress.env('NEXT_PUBLIC_DOMAIN')}/users`)
+describe('page security', () => {
+  it('page security', () => {
+    cy.visit('/users')
+    cy.location('pathname').should('eq', '/');
   })
 })
 
-describe('users page functionality', () => {
+describe('page functions', () => {
+  const testEmail       = Cypress.env('TEST_EMAIL')
+  const testPassword    = Cypress.env('TEST_PASSWORD')
+
   beforeEach(() => {
-    cy.login(Cypress.env('EMAIL'), Cypress.env('PASSWORD'));
-    cy.url().should('eq', `${Cypress.env('NEXT_PUBLIC_DOMAIN')}/users`);
+    cy.login(testEmail, testPassword);
   });
-  it('redirects users to main page after logout', () => {
-    cy.get('.bottom-0 > [href="/#"]').click()
-    cy.url().should('eq', `${Cypress.env('NEXT_PUBLIC_DOMAIN')}/`)
-  })
-  it('allows users to navigate to conversations', () => {
-    cy.get('.bottom-0 > [href="/conversations"]').click()
-    cy.url().should('eq', `${Cypress.env('NEXT_PUBLIC_DOMAIN')}/conversations`)
+
+  it('users page UX', () => {
+    cy.get('a#mobileItem[href="/conversations"]').click()
+    cy.location('pathname').should('eq', '/conversations');
+
+    cy.get('a#mobileItem[href="/#"]').click()
+    cy.location('pathname').should('eq', '/');
   })
 
-  it('allows users to create conversations and redirects user', () => {
-    cy.intercept('POST', `${Cypress.env('NEXT_PUBLIC_DOMAIN')}/api/conversations`).as('CreateConversations')
-
-    cy.get('.px-5 > :nth-child(2)').click()
+  it('create conversation', () => {
+    const conversationAPI = 'api/conversations'
+    cy.intercept('POST', conversationAPI).as('CreateConversations')
+    cy.get('div#userBox').first().click()
     cy.wait('@CreateConversations').then((intercept) => {
       expect(intercept.response?.statusCode).to.equal(200);
       const conversationId = intercept.response.body.id
-      cy.url().should('eq', `${Cypress.env('NEXT_PUBLIC_DOMAIN')}/conversations/${conversationId}`)
+      cy.location('pathname').should('eq', `/conversations/${conversationId}`);
     })
   })
 })
