18f613cbc5d7ee34aa698ed67c85ef81b197cf2a
Zhong Tan <zhtgeneral@gmail.com>
2024-09-08 13:07:36 -0700

    fixed build errors

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,5 +1,8 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+// import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+
+
 import { Session, getServerSession } from 'next-auth'
+import authOptions from '../libs/authOptions';
 
 export default async function getSession(): Promise<Session | null> {
   return await getServerSession(authOptions);
--- cypress/e2e/users.cy.ts
+++ cypress/e2e/users.cy.ts
@@ -18,8 +18,24 @@ describe('page functions', () => {
     cy.intercept('POST', conversationAPI).as('CreateConversations')
     cy.get('div#userBox').first().click()
     cy.wait('@CreateConversations').then((intercept) => {
-      expect(intercept.response?.statusCode).to.equal(200);
-      const conversationId = intercept.response.body.id
+      expect(intercept).to.exist
+      expect(intercept.response).to.exist
+
+      // TypeScript type assertion
+      const intercept_t = intercept as { 
+        response: { 
+          statusCode: number;
+           body: { 
+            id: string 
+          }
+        } 
+      }
+
+      expect(intercept_t.response.statusCode).to.equal(200)
+      expect(intercept_t.response.body).to.exist
+      expect(intercept_t.response.body.id).to.exist
+
+      const conversationId = intercept_t.response.body.id
       cy.location('pathname').should('eq', `/conversations/${conversationId}`);
     })
   })
