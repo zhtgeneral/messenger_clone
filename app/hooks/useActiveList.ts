fdf07673f6725d92e3e3fb635b597b4e009421e6
Zhong <zhtgeneral@gmail.com>
2024-10-11 14:23:21 -0700

    docs

--- app/hooks/useActiveList.ts
+++ app/hooks/useActiveList.ts
@@ -1,19 +1,27 @@
-import {create} from 'zustand'
-
-// as usual the docs are here https://docs.pmnd.rs/zustand/getting-started/introduction
+import { create } from 'zustand'
 
 interface ActiveListStore {
   members: string[],
-  add    : (id : string  ) => void,
-  remove : (id : string  ) => void,
-  set    : (ids: string[]) => void
+  add: (id : string) => void,
+  remove: (id : string) => void,
+  set: (ids: string[]) => void
 }
 
+/**
+ * This store keeps track of the global state of members.
+ * 
+ * A member is added if they are subscribed to Presence Channels using Pusher.
+ * 
+ * A member is removed if the window of the app is closed.
+ * 
+ * @link https://docs.pmnd.rs/zustand/getting-started/introduction
+ * 
+ */
 const useActiveList = create<ActiveListStore>((set) => ({
   members: [],
-  add    : (id)  => set((state) => ({members: [...state.members, id]})),
-  remove : (id)  => set((state) => ({members: state.members.filter((memberId) => memberId != id)})),
-  set    : (ids) => set({members: ids})
-}))
+  add: (id)  => set((state) => ({members: [...state.members, id]})),
+  remove: (id)  => set((state) => ({members: state.members.filter((memberId) => memberId != id)})),
+  set: (ids) => set({members: ids})
+}));
 
-export default useActiveList
+export default useActiveList;
